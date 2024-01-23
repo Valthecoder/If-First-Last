0822cd5e8a8ecd830c153998cb5cfea39ac80bef
ValtheCoder <keshia.kdf@gmail.com>
2024-01-23 18:08:02 +0800

    Given list phrase added

--- CheckFirst=Last.py
+++ CheckFirst=Last.py
@@ -3,12 +3,12 @@ numbers_x = [10, 20, 30, 40, 10]
 numbers_y = [75, 65, 35, 75, 30]
 # If  Function
 if numbers_x[0] == numbers_x[-1]:
-    print(numbers_x, "result is True")
+    print("Given list: ", numbers_x, "result is True")
 else:
-    print(numbers_x, "result is False")
+    print("Given List: ", numbers_x, "result is False")
 
 if numbers_y[0] == numbers_y[-1]:
-    print (numbers_y, "result is True")
+    print ("Given List: ", numbers_y, "result is True")
 else:
-    print(numbers_y, "result is False")
+    print("Given List: ", numbers_y, "result is False")
 
