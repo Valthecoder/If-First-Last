853d102b2942092cdf02a2de4c013a531c6309d7
ValtheCoder <keshia.kdf@gmail.com>
2024-01-23 18:06:11 +0800

    No need list function, added if else function

--- CheckFirst=Last.py
+++ CheckFirst=Last.py
@@ -1,5 +1,14 @@
 # Type the given
 numbers_x = [10, 20, 30, 40, 10]
 numbers_y = [75, 65, 35, 75, 30]
-# If and return Functiongi
-# List Function
+# If  Function
+if numbers_x[0] == numbers_x[-1]:
+    print(numbers_x, "result is True")
+else:
+    print(numbers_x, "result is False")
+
+if numbers_y[0] == numbers_y[-1]:
+    print (numbers_y, "result is True")
+else:
+    print(numbers_y, "result is False")
+
